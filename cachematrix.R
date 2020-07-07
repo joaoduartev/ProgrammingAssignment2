1227db96adece16e409f09c8c481ef38eb7a2e2a
joaoduartev <joaovpduarte@gmail.com>
2020-07-07 00:37:36 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Basically the first function defines a list with three functions
+# The list is what is returned when the function is called
+# The second function calls the three function previously defined to apply solve() to the data
 
-## Write a short comment describing this function
+# dados() calls the matrix in the argument
+# inversa() calls the variable that will store the matrix resulted from solve() function
+# calc_inversa calls solve() and applies it to the original matrix (argument of the function)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matriz){
+    resposta <- c()
+    dados <- function() matriz
+    inversa <- function() resposta
+    calc_inversa <- function(solve) resposta <<- solve
+    list(dados = dados, calc_inversa = calc_inversa, inversa = inversa)
 }
 
+# This function is the one which actually calculates the inverse matrix
+# inversa() calls the variable that will store the matrix resulted from solve() function
+# calc_inversa() calls solve() and applies it to the original matrix (argument of the function)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matriz){
+    resposta <- matriz$inversa()
+    if(length(resposta)!=0){
+        message("Using already computed data")
+        resposta
+    }
+    dados <- matriz$dados()
+    resposta <- solve(dados)
+    matriz$calc_inversa(resposta)
+    resposta
 }
