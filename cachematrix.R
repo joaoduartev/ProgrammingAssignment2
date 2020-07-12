524f429bf07c974241b84a9f49bdabc010b023a9
joaoduartev <joaovpduarte@gmail.com>
2020-07-12 01:16:45 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,31 @@
-# Basically the first function defines a list with three functions
+# Basically th first function defines a list with three functions
 # The list is what is returned when the function is called
 # The second function calls the three function previously defined to apply solve() to the data
 
-# dados() calls the matrix in the argument
-# inversa() calls the variable that will store the matrix resulted from solve() function
-# calc_inversa calls solve() and applies it to the original matrix (argument of the function)
+# datas() calls the matrix in the argument
+# inverse() calls the variable that will store the matrix resulted from solve() function
+# calc_inverse calls solve() and applies it to the original matrix (argument of the function)
 
 makeCacheMatrix <- function(matriz){
-    resposta <- c()
-    dados <- function() matriz
-    inversa <- function() resposta
-    calc_inversa <- function(solve) resposta <<- solve
-    list(dados = dados, calc_inversa = calc_inversa, inversa = inversa)
+    answer <- c()
+    datas <- function() matriz
+    inverse <- function() answer
+    calc_inverse <- function(solve) answer <<- solve
+    list(datas = datas, calc_inverse = calc_inverse, inverse = inverse)
 }
 
 # This function is the one which actually calculates the inverse matrix
-# inversa() calls the variable that will store the matrix resulted from solve() function
-# calc_inversa() calls solve() and applies it to the original matrix (argument of the function)
+# inverse() calls the variable that will store the matrix resulted from solve() function
+# calc_inverse() calls solve() and applies it to the original matrix (argument of the function)
 
 cacheSolve <- function(matriz){
-    resposta <- matriz$inversa()
-    if(length(resposta)!=0){
+    answer <- matriz$inverse()
+    if(length(answer)!=0){
         message("Using already computed data")
-        resposta
+        answer
     }
-    dados <- matriz$dados()
-    resposta <- solve(dados)
-    matriz$calc_inversa(resposta)
-    resposta
+    datas <- matriz$datas()
+    answer <- solve(datas)
+    matriz$calc_inverse(answer)
+    answer
 }
